38014f03f7499415b58d212d980ea90a4408c37d
cartesinus <cartesinus@gmail.com>
2020-02-16 21:04:37 +0100

    add omegat en2pl project
    make slot language dependent
    
    add pl-PL slots for websearch

--- scripts/expand-slots.sh
+++ scripts/expand-slots.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-INPUT_FILE=$1
+LANG=$1
+INPUT_FILE=$2
 
-for slot in `ls -1 slots/`; do
+for slot in `ls -1 slots/${LANG}`; do
     if grep --quiet "$slot" $INPUT_FILE; then
         echo "expanding with phrases from $slot."
         while IFS= read -r text; do
             sed -ie "0,/$slot/ s/$slot/$text/" $INPUT_FILE
-        done < slots/$slot
+        done < slots/$LANG/$slot
     fi;
 done
