4a2cf4db8d4f1aee4d7870548147c398210b4892
cartesinus <cartesinus@gmail.com>
2020-02-18 23:05:29 +0100

    move almost all slot to external files

--- scripts/expand-slots.sh
+++ scripts/expand-slots.sh
@@ -2,12 +2,24 @@
 
 LANG=$1
 INPUT_FILE=$2
+REPEAT=${3:-"true"}
+
+function substitute_slots {
+    slot=$1
+    while IFS= read -r text; do
+        sed -ie "0,/$slot/ s/$slot/$text/" $INPUT_FILE
+    done < slots/$LANG/$slot
+}
 
 for slot in `ls -1 slots/${LANG}`; do
     if grep --quiet "$slot" $INPUT_FILE; then
         echo "expanding with phrases from $slot."
-        while IFS= read -r text; do
-            sed -ie "0,/$slot/ s/$slot/$text/" $INPUT_FILE
-        done < slots/$LANG/$slot
+        if [ "$REPEAT" = "true" ]; then
+            while grep --quiet "$slot" $INPUT_FILE; do
+                substitute_slots $slot
+            done
+        else
+            substitute_slots $slot
+        fi;
     fi;
 done
