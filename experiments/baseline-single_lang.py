482ce922da33095c214c06bf0b5c57fb353f4c03
cartesinus <cartesinus@gmail.com>
2020-04-19 23:57:54 +0200

    add scripts for baseline experiment

---
+++ experiments/baseline-single_lang.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""Replicate baseline models for en, es and pl.
+
+This script, designed to be run on colab, will convert leyzer corpora to atis format and train
+3 baseline models.
+"""
+
+!pip install nemo-nlp
+!git clone https://github.com/NVIDIA/NeMo
+!pip install -r NeMo/requirements/requirements_nlp.txt
+!cd NeMo && ./reinstall.sh
+
+# convert corpora to atis format
+!git clone https://github.com/cartesinus/leyzer
+
+for lang in ["en-US", "es-ES", "pl-PL"]:
+    !mkdir data/baseline/"$lang"
+    # create dictionary
+    !./scripts/convert-to-nemo.py --create_dictionary \
+        ../corpora/leyzer_dev-en_US-0.1.0.tsv \
+        ../corpora/leyzer_train-en_US-0.1.0.tsv --output data/baseline/"$lang"/
+
+    # create trainset in atis format
+    !./convert-to-nemo.py --input ../corpora/leyzer_train-"$lang"-0.1.0.tsv \
+        -n data/baseline/leyzer.dict.intent.csv \
+        -t data/baseline/leyzer.dict.vocab.csv \
+        -s data/baseline/leyzer.dict.slots.csv \
+        --part train
+        --output data/baseline/"$lang"/
+
+    # create testset in atis format
+    !./convert-to-nemo.py --input ../corpora/leyzer_test-"$lang"-0.1.0.tsv \
+        -n data/baseline/leyzer.dict.intent.csv \
+        -t data/baseline/leyzer.dict.vocab.csv \
+        -s data/baseline/leyzer.dict.slots.csv \
+        --part test
+        --output data/baseline/"$lang"/
+
+    # train baseline
+    !cd NeMo && python examples/nlp/intent_detection_slot_tagging/joint_intent_slot_with_bert.py \
+        --data_dir ./data/baseline/"$lang"/ \
+        --work_dir ./data/baseline/"$lang"/model \
+        --max_seq_length 21 \
+        --num_epochs 50 \
+        --optimizer_kind adam
+
