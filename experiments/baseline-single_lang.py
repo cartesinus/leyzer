851a226d22e860573773fd2215827be4fa8a97fe
cartesinus <cartesinus@gmail.com>
2020-04-20 20:44:44 +0200

    fix baseline experiment script

--- experiments/baseline-single_lang.py
+++ experiments/baseline-single_lang.py
@@ -10,36 +10,14 @@ To convert this script to ipynb you can use https://pypi.org/project/ipynb-py-co
 !pip install nemo-nlp
 !git clone https://github.com/NVIDIA/NeMo
 !pip install -r NeMo/requirements/requirements_nlp.txt
-!cd NeMo && ./reinstall.sh
+!cd NeMo && git reset --hard ac4a765 && ./reinstall.sh
 
+!./leyzer/scripts/convert-to-nemo.sh leyzer ./
 # convert corpora to atis format
 !git clone https://github.com/cartesinus/leyzer
 
+# train baseline
 for lang in ["en-US", "es-ES", "pl-PL"]:
-    lang_under = lang.replace('-', '_')
-    !mkdir -p data/baseline/"$lang"
-    # create dictionary
-    !./leyzer/scripts/convert-to-nemo.py --create_dictionary \
-        ./leyzer/corpora/leyzer_test-"$lang_under"-0.1.0.tsv \
-        ./leyzer/corpora/leyzer_train-"$lang_under"-0.1.0.tsv --output data/baseline/"$lang"/
-
-    # create trainset in atis format
-    !./leyzer/scripts/convert-to-nemo.py --input ./leyzer/corpora/leyzer_train-"$lang_under"-0.1.0.tsv \
-        -n data/baseline/"$lang"/leyzer.dict.intent.csv \
-        -t data/baseline/"$lang"/leyzer.dict.vocab.csv \
-        -s data/baseline/"$lang"/leyzer.dict.slots.csv \
-        --part train \
-        --output data/baseline/"$lang"/
-
-    # create testset in atis format
-    !./leyzer/scripts/convert-to-nemo.py --input ./leyzer/corpora/leyzer_test-"$lang_under"-0.1.0.tsv \
-        -n data/baseline/"$lang"/leyzer.dict.intent.csv \
-        -t data/baseline/"$lang"/leyzer.dict.vocab.csv \
-        -s data/baseline/"$lang"/leyzer.dict.slots.csv \
-        --part test \
-        --output data/baseline/"$lang"/
-
-    # train baseline
     !cd NeMo && python examples/nlp/intent_detection_slot_tagging/joint_intent_slot_with_bert.py \
         --data_dir ../data/baseline/"$lang"/ \
         --work_dir ../data/baseline/"$lang"/model \
---
+++ scripts/convert-to-nemo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+
+LASER_PATH=$1
+OUTPUT_PATH=$2
+
+for lang in "en-US" "es-ES" "pl-PL"; do
+    mkdir -p ${OUTPUT_PATH}/data/baseline/${lang}
+
+    #create dictionary
+    cut -f2 ${LEYZER_PATH}/corpora/0.1.0/leyzer-dev-${lang}-0.1.0.tsv \
+        ${LEYZER_PATH}/corpora/0.1.0/leyzer-test-${lang}-0.1.0.tsv \
+        ${LEYZER_PATH}/corpora/0.1.0/leyzer-train-${lang}-0.1.0.tsv | sort -u | \
+        sed '/^intent$/d' > ${OUTPUT_PATH}/data/baseline/${lang}/dict.intents.csv
+
+    cut -f4 ${LEYZER_PATH}/corpora/0.1.0/leyzer-dev-${lang}-0.1.0.tsv \
+        ${LEYZER_PATH}/corpora/0.1.0/leyzer-test-${lang}-0.1.0.tsv \
+        ${LEYZER_PATH}/corpora/0.1.0/leyzer-train-${lang}-0.1.0.tsv | tr ' ' '\n' | \
+        sort -u | sed '/^bio$/d' | sed '/^o$/g' | sed 's/^i-/I-/g' | \
+        sed 's/^b-/B-/g' | sed '1 i O' > ${OUTPUT_PATH}/data/baseline/${lang}/dict.slots.csv
+
+    #reformat corpus
+    for part in "train" "test" "dev"; do
+        paste <(cut -f3 ${LEYZER_PATH}/corpora/0.1.0/leyzer-${part}-${lang}-0.1.0.tsv | tr '[A-Z]' '[a-z]') \
+            <(cut -f2 ${LEYZER_PATH}/corpora/0.1.0/leyzer-${part}-${lang}-0.1.0.tsv) | \
+            sed 's/utterance\tintent/sentence\tlabel/g' > ${OUTPUT_PATH}/data/baseline/${lang}/${part}.tsv
+
+        cut -f4 ${LEYZER_PATH}/corpora/0.1.0/leyzer-${part}-${lang}-0.1.0.tsv | \
+            sed 's/ o / O /g' | sed 's/ o / O /g' | sed 's/^o /O /g' | sed 's/ o$/ O/g' | \
+            sed 's/b-/B-/g' | sed 's/i-/I-/g' | \
+            sed '1d' > ${OUTPUT_PATH}/data/baseline/${lang}/${part}_slots.tsv
+    done
+done
