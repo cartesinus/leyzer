6b3468697d29e22e5d8cfb2ced87b0995632cb74
piotrek <piotrek@bolid2.localdomain>
2022-09-26 18:50:59 +0200

    Add target directory

--- gen_all_langs.sh
+++ gen_all_langs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+TARGET_DIR=${1:-all_langs}
 
 for lang in en-US es-ES pl-PL
 do
-    domains=grammars/${lang}/[!console]*.gram
+    domains=grammars/${lang}/*.gram
     for domain in ${domains}
     do
         domain=$(basename $domain .gram)
         python3 JSGFTools/DeterministicGenerator.py grammars/${lang}/${domain}.gram > patterns/${lang}/${domain}.tsv
-        mkdir -p corpora/all_langs/${lang}
-        ./scripts/expand-slots.sh ${lang} patterns/${lang}/${domain}.tsv corpora/all_langs/${lang}/${domain}.tsv true true
+        mkdir -p corpora/${TARGET_DIR}/${lang}
+        ./scripts/expand-slots.sh ${lang} patterns/${lang}/${domain}.tsv corpora/${TARGET_DIR}/${lang}/${domain}.tsv true true
     done
 done
