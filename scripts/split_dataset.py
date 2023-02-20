7cc0a1d12c73939b0647a8e8aecc078144c6046d
cartesinus <msowansk@gmail.com>
2023-02-20 17:32:14 +0100

    adding slot values to pl-PL

--- scripts/split_dataset.py
+++ scripts/split_dataset.py
@@ -16,12 +16,16 @@ import os
 import math
 
 
-def save_file(dataset, filename):
+def save_file(dataset, filename, bio):
     """ Writte csv to file. """
     with open(filename, 'w', newline='') as file:
         corpus_out = csv.writer(file, delimiter="\t", quoting=csv.QUOTE_MINIMAL)
-        for t in dataset:
-            corpus_out.writerow(t)
+        if bio:
+            for domain, intent, level, vp, utt_bio in dataset:
+                corpus_out.writerow([domain, intent, level, vp, utt_bio[0], utt_bio[1]])
+        else:
+            for t in dataset:
+                corpus_out.writerow(t)
 
     return True
 
@@ -36,6 +40,9 @@ if __name__ == '__main__':
                         help='Output file for test part of corpus.')
     argparse.add_argument('--stats', dest='stats', action='store_true')
     argparse.add_argument('--no-stats', dest='stats', action='store_false')
+    argparse.add_argument('--with-bio', dest='wbio', action='store_true')
+    argparse.add_argument('--without-bio', dest='wbio', action='store_false')
+    argparse.add_argument('--corpus_version', dest='corpver', default='0.2.0')
     argparse.set_defaults(stats=True)
 
     args = argparse.parse_args()
@@ -47,15 +54,36 @@ if __name__ == '__main__':
         with open(args.filename, newline='') as dataset:
             reader = csv.reader(dataset, delimiter="\t")
 
-            for domain, intent, level, pattern, utterance in reader:
-                uniq_pattern_idx = "#".join([domain, intent, level, pattern])
-                if uniq_pattern_idx in corpus:
-                    corpus[uniq_pattern_idx].append(utterance)
+            if args.corpver.startswith("0.2"):
+            #in 0.2.0 additional columns were added: level and verb pattern
+                if args.wbio:
+                    for domain, intent, level, pattern, utterance, bio in reader:
+                        uniq_pattern_idx = "#".join([domain, intent, level, pattern])
+                        if uniq_pattern_idx in corpus:
+                            corpus[uniq_pattern_idx].append([utterance, bio])
+                        else:
+                            corpus[uniq_pattern_idx] = [[utterance, bio]]
                 else:
-                    corpus[uniq_pattern_idx] = [utterance]
+                    for domain, intent, level, pattern, utterance in reader:
+                        uniq_pattern_idx = "#".join([domain, intent, level, pattern])
+                        if uniq_pattern_idx in corpus:
+                            corpus[uniq_pattern_idx].append(utterance)
+                        else:
+                            corpus[uniq_pattern_idx] = [utterance]
 
-            if args.stats:
-                print("Domain\tIntent\tLevel\tVerbPattern\tTotalPatterns\tTrainPatterns\tTestPatterns")
+                if args.stats:
+                    print("Domain\tIntent\tLevel\tVerbPattern\tTotalPatterns\tTrainPatterns\tTestPatterns")
+
+            else:
+                for domain, intent, utterance in reader:
+                    uniq_pattern_idx = "#".join([domain, intent])
+                    if uniq_pattern_idx in corpus:
+                        corpus[uniq_pattern_idx].append(utterance)
+                    else:
+                        corpus[uniq_pattern_idx] = [utterance]
+
+                if args.stats:
+                    print("Domain\tIntent\tTotalPatterns\tTrainPatterns\tTestPatterns")
 
             for pattern in corpus:
                 total_patterns = len(corpus[pattern])
@@ -73,7 +101,8 @@ if __name__ == '__main__':
                 if args.stats:
                     print("\t".join(pattern.split("#") + [str(total_patterns), str(train_size), str(test_size)]))
 
-        save_file(test_corpus, args.test)
-        save_file(train_corpus, args.train)
+
+        save_file(test_corpus, args.test, args.wbio)
+        save_file(train_corpus, args.train, args.wbio)
     else:
         print("No file was provided.")
