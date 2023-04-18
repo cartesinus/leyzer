dcac938921f0b270389d6fa7297a2304b6442115
matgora <mgora1234@gmail.com>
2023-04-18 19:10:44 +0200

    fix date slot in calendar domain

--- scripts/convert_str_to_bio.py
+++ scripts/convert_str_to_bio.py
@@ -3,6 +3,7 @@
 
 import sys
 
+DATE_SLOTS = ['date_na', 'date_w0', 'date_we']
 sentences = sys.stdin.readlines()
 for sentence in sentences:
     bio = ""
@@ -14,11 +15,15 @@ for sentence in sentences:
         if word.startswith('{') and word.endswith('}'):
             in_slot = False
             slot = word.split(':')[0][1:].lower()
+            if slot in DATE_SLOTS:
+                slot = 'date'
             bio += "b-" + slot + " "
             raw_sentence += word.split(':')[1][:-1] + " "
         elif word.startswith('{'):
             in_slot = True
             slot = word.split(':')[0][1:].lower()
+            if slot in DATE_SLOTS:
+                slot = 'date'
             bio += "b-" + slot + " "
             raw_sentence += word.split(':')[1] + " "
         elif word.endswith('}'):
